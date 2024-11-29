2ea3fb5d4a59d67698f301f3efc852ae1a0944c1
Ashish <ashish@192.168.1.9>
2024-11-29 18:19:08 +0530

    some error resolved

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -48,7 +48,7 @@ router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvat
 
 router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
 
-Pouter, route("/c/:username").get(verifyJWT, getUserChannelProfile)
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
 
 router.route("/history").get(verifyJWT, getWatchHistory)
 
